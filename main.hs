4a1e6c75ab3f780c46031ccc4ca27b78ecaf0664
F1948 <muzupload@ya.ru>
2020-07-01 17:40:09 +0300

    playing with args in main.hs

--- main.hs
+++ main.hs
@@ -1,7 +1,15 @@
 module Main where
 import System.Environment
 
+f :: [String] -> [Int]
+f x = case x of 
+  [] -> [0, 0]
+  (a:[]) -> [(read a), 0]
+  b -> map read b
+
 main :: IO ()
 main = do
   args <- getArgs
-  putStrLn ("Hello, " ++ args !! 0)
+  let args1 = f args
+  let result = args1 !! 0 + args1 !! 1 
+  putStrLn ("Hello, " ++ show result)
